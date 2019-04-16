9c98c7da12cb14d37a65b54eecd4bd8b00829b11
Maddie Jones <maddyg91@gmail.com>
2019-04-16 10:31:40 -0600

    Adds grid to favorite index page

--- favorites_index.js
+++ favorites_index.js
@@ -9,38 +9,43 @@ function getFavorite() {
 function renderFavorite(data) {
   favorites = data["data"]
   for (let favorite of favorites) {
+    let id = favorite.id
     let media_type = favorite["attributes"].media_type
+    container = document.createElement('section');
+    container.id = `fav-container-${id}`;
+    container.className = `fav-container`;
+    document.getElementById("favorites").appendChild(container)
     if (media_type == "video") {
       let url = favorite["attributes"].url
       video = document.createElement('iframe');
       video.src = url;
       video.className = 'fav-video';
-      document.getElementById("favorites").appendChild(video);
+      document.getElementById(`fav-container-${id}`).appendChild(video);
     } else if (media_type == "image") {
       var url = favorite["attributes"].url
       image = document.createElement('img');
       image.src = url;
       image.className = 'fav-image';
-      document.getElementById("favorites").appendChild(image);
+      document.getElementById(`fav-container-${id}`).appendChild(image);
     };
     let imageTitle = favorite["attributes"].title
     title = document.createElement('p');
     title.innerHTML = imageTitle;
     title.className = 'fav-title';
-    document.getElementById("favorites").appendChild(title);
+    document.getElementById(`fav-container-${id}`).appendChild(title);
 
     let imageExplanation = favorite["attributes"].explanation
     explanation = document.createElement('p');
     explanation.innerHTML = imageExplanation;
     explanation.className = 'fav-explanation';
-    document.getElementById("favorites").appendChild(explanation);
+    document.getElementById(`fav-container-${id}`).appendChild(explanation);
 
     deleteBtn = document.createElement('button');
     deleteBtn.innerHTML = "delete favorite" ;
     deleteBtn.className = 'delete-btn';
     deleteBtn.addEventListener('click',function() {
        removeFromFavorites(data)});
-    document.getElementById("favorites").appendChild(deleteBtn);
+    document.getElementById(`fav-container-${id}`).appendChild(deleteBtn);
   }
 }
 
